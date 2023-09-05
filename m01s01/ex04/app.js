0d7aa9945f4a5d552e2598dfd45fe1406ea9f3c0
George Ion <89georgeion@gmail.com>
2023-09-05 19:10:55 +0300

    tema m1s1e3-4-5

---
+++ m01s01/ex04/app.js
@@ -0,0 +1,18 @@
+var inputRadius = document.getElementById('radius');
+var elementResult = document.getElementById('result');
+var form = document.querySelector('form');
+
+form.addEventListener(
+  'submit',
+  function (event) {
+    event.preventDefault();
+
+    var radius = inputRadius.value || 0;
+    var result = 0;
+
+    result = radius * radius * Math.PI;
+
+    elementResult.innerText = result;
+  },
+  false,
+);
