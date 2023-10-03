135bdf6aa9f3054bec64babed56be5cc15f0fe1f
George Ion <89georgeion@gmail.com>
2023-10-03 22:01:34 +0300

    m2s1e1-2-3

---
+++ m02s01/ex01/app.js
@@ -0,0 +1,46 @@
+//  syntactical sugar
+// js nu are clase
+class Car {
+  // notatia 1
+  topSpeed = 160;
+  topReverseSpeed = -50;
+  constructor(make, color, wheels, speed) {
+    this.make = make;
+    this.color = color;
+    this.speed = speed;
+    this.wheels = wheels;
+  }
+
+  accelerate() {
+    this.speed++;
+  }
+
+  decelerate() {
+    this.speed--;
+  }
+
+  stop() {
+    this.speed = 0;
+
+    console.log(`Viteza noua este ${this.speed}.`);
+  }
+
+  setSpeed(speed) {
+    if (speed > this.topSpeed) {
+      speed = this.topSpeed;
+    }
+    if (speed < this.topReverseSpeed) {
+      speed = this.topReverseSpeed;
+    }
+
+    this.speed = speed;
+  }
+}
+
+const audi = new Car('Audi', 'black', 4, 50);
+
+const opel = new Car('Opel', 'red', 4, 3);
+
+const cars = [audi, opel];
+
+// sa vedem bucla pe tema
---
+++ m02s01/ex02/app.js
@@ -0,0 +1,70 @@
+class Vehicle {
+  constructor(
+    make,
+    color,
+    wheels,
+    speed,
+    topSpeed = 160,
+    topReverseSpeed = -50,
+  ) {
+    this.make = make;
+    this.color = color;
+    this.speed = speed;
+    this.wheels = wheels;
+    this.topSpeed = topSpeed;
+    this.topReverseSpeed = topReverseSpeed;
+  }
+
+  displaySpeed() {
+    console.log(`Viteza curenta este: ${this.speed}.`);
+  }
+
+  accelerate() {
+    this.setSpeed(this.speed + 1);
+  }
+
+  decelerate() {
+    this.setSpeed(this.speed - 1);
+  }
+
+  setSpeed(speed = 5) {
+    if (speed > this.topSpeed) {
+      speed = this.topSpeed;
+    }
+
+    if (speed < this.topReverseSpeed) {
+      speed = this.topReverseSpeed;
+    }
+
+    this.speed = speed;
+    this.displaySpeed();
+  }
+}
+
+class Car extends Vehicle {
+  constructor(make, color, speed, topSpeed, topReverseSpeed) {
+    super(make, color, 4, speed, topSpeed, topReverseSpeed);
+  }
+}
+
+class Bicycle extends Vehicle {
+  constructor(make, color, speed, topSpeed) {
+    super(make, color, 2, speed, topSpeed, 0);
+  }
+
+  // ex naiv de polimorfism
+  // decelerate() {
+  //   if (--this.speed < 0) {
+  //     this.speed = 0;
+  //   }
+  //   this.displaySpeed();
+  // }
+}
+
+const bike = new Bicycle('Pegas', 'red', 8, 20);
+
+bike.setSpeed(2);
+
+bike.decelerate();
+bike.decelerate();
+bike.decelerate();
