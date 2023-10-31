c4a5d3c173f68e30a84c3468957a96b5d2d1f6a9
George Ion <89georgeion@gmail.com>
2023-10-31 21:58:15 +0200

    m2s4e4 m2s5e1-2-4

---
+++ m02s05/ex01/app.js
@@ -0,0 +1,6 @@
+const glide = new Glide('.glide', {
+  autoplay: 2000,
+  type: 'carousel',
+});
+
+glide.mount();
---
+++ m02s05/ex04/app.js
@@ -0,0 +1,27 @@
+const blueprint = document.querySelector('.alert');
+
+function showAlert(message) {
+  const clone = blueprint.cloneNode(true);
+  const paragraph = clone.querySelector('p');
+
+  paragraph.innerText = message;
+
+  document.body.append(clone);
+
+  clone.open = true;
+}
+
+// super bad practice
+window.alert = showAlert;
+
+document.body.addEventListener('submit', function (event) {
+  const form = event.target;
+
+  if (form.nodeName !== 'FORM' || !form.classlist.contains('alert-close')) {
+    return;
+  }
+
+  event.preventDefault();
+
+  form.parentElement.remove();
+});
