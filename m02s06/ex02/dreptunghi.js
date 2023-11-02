73631df724549b1e68cd9bf59addbd79431bef5d
George Ion <89georgeion@gmail.com>
2023-11-02 22:04:15 +0200

    M2S6E1-2-3-5-7

---
+++ m02s06/ex02/dreptunghi.js
@@ -0,0 +1,8 @@
+console.log(process.argv);
+// descructurare
+// let length = process.argv[2]
+// let width = process.argv[3]
+let [, , length = 1, width = 1] = process.argv;
+length = Number(length);
+width = Number(width);
+console.log(length * width);
